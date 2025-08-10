bd3b3ad0296e708cea459762d0c997654b976203
ChloeSVillaranda <chloesvillaranda@gmail.com>
2025-08-10 12:19:36 -0600

    feat: favicons and logo chaged from default react to ucalgary trivia club icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,16 @@
-import { Head, Html, Main, NextScript } from 'next/document';
-
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <link rel="icon" href="/favicon.ico?v=3" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=3" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=3" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3" />
+          <link rel="manifest" href="/manifest.json?v=3" />
+          
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
